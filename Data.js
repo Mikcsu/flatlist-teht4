68cbbe49afa909ca7fe0776f5b289b32b914e496
Mikcsu <miika.puominen@gmail.com>
2024-02-05 14:57:47 +0200

    teht4 valamis

---
+++ Data.js
@@ -0,0 +1,30 @@
+
+
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Juntunen',
+        firstname: 'jouni'
+    },
+    {
+        id: '2',
+        lastname: 'Puominen',
+        firstname: 'Miika'
+    },
+    {
+        id: '3',
+        lastname: 'Juntunen',
+        firstname: 'pete'
+    },
+    {
+        id: '4',
+        lastname: 'Puominen',
+        firstname: 'Marttin'
+    },
+    {
+        id: '5',
+        lastname: 'Petäjä',
+        firstname: 'Marko'
+    },
+]
+
